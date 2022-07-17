05a3cb5eff11feed7d027bbb3858a57cb21676af
Hemant <hrsmart2150@gmail.com>
2022-07-17 11:32:59 +0530

    Completed header part

---
+++ components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <h1 className='max-w-xl text-[6rem] font-mediumSerif ' >Stay Curious.</h1>
+    </div>
+  )
+}
+
+export default Banner
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import logo from '../public/static/logo.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='wrapper flex justify-center gap-10 p-5 bg-[#FCC017]  ' >
+        <div className='content max-w-7xl flex-1 flex justify-between gap-10  '>
+            <div className='logoContainer flex items-center h-[40px] w-[200px] '>
+                <Image className='logo cursor-pointer' objectFit='contain' src={logo} alt='logo' />
+            </div>
+            <div className='bannerNav flex items-center space-x-5 ' >
+                <div className='cursor-pointer' >Our Story</div>
+                <div className='cursor-pointer' >Membership</div>
+                <div className='cursor-pointer' >Sign In</div>
+                <button className='accentedButton bg-black text-white py-2 px-4 rounded-full ' >Get Started</button>
+            </div>
+        </div>
+    </header>
   )
 }
 
